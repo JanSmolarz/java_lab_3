989c3cce93215e75d32df2633bd649ce6959c7c3
Jan <jan.smolarz@yahoo.com>
2022-01-28 22:58:42 +0100

    zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
+
+enum LiczbyEnum{
+    JEDEN, DWA, TRZY, CZTERY, PIĘĆ, SZEŚĆ
+}
+
+enum StatusEnum{
+    KONTYNUUJEMY, KONIEC
+}
+
 
 public class Main {
 
@@ -22,13 +30,6 @@ public class Main {
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
-        enum LiczbyEnum{
-            JEDEN, DWA, TRZY, CZTERY, PIĘĆ, SZEŚĆ;
-        }
-
-        enum StatusEnum{
-            KONTYNUUJEMY, KONIEC;
-        }
 
         Scanner scan = new Scanner(System.in);
         boolean petla = true;
@@ -41,7 +42,36 @@ public class Main {
             if (x == 0){
                break;
             }
-        }
+            switch (x){
+                case 0:
+                    System.out.println(StatusEnum.KONIEC.toString());
+                    break;
+                case 1:
+                    System.out.println(LiczbyEnum.JEDEN.toString());
+                    System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+                    break;
+                case 2:
+                    System.out.println(LiczbyEnum.DWA.toString());
+                    System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+                    break;
+                case 3:
+                    System.out.println(LiczbyEnum.TRZY.toString());
+                    System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+                    break;
+                case 4:
+                    System.out.println(LiczbyEnum.CZTERY.toString());
+                    System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+                    break;
+                case 5:
+                    System.out.println(LiczbyEnum.PIĘĆ.toString());
+                    System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+                    break;
+                case 6:
+                    System.out.println(LiczbyEnum.SZEŚĆ.toString());
+                    System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+                    break;
+
+            }
 
     }
-}
+}}
